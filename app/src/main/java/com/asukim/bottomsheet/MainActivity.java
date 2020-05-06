0d7000a60cdd430e8bf0dd5da13c7ad987eaba04
asuKim <asukim1022@gmail.com>
2020-05-07 00:40:20 +0900

    BottomSheet

---
+++ app/src/main/java/com/asukim/bottomsheet/MainActivity.java
@@ -0,0 +1,78 @@
+package com.asukim.bottomsheet;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomSheetDialog mBottomSheetDialog;
+    BottomSheetBehavior behavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //bottomSheet : bottomSheet을 사용하기 위해 선언
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+
+        //bottomSheetBtn : 클릭시 showBottomSheetDialog() 띄우기
+        Button bottomSheetBtn = (Button) findViewById(R.id.bottomSheetBtn);
+        bottomSheetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
+
+    }
+
+    /** @brief showBottomSheetDialog
+     *  @date 2016-02-18
+     *  @detail 상세보기 bottomSheetDialog 띄우기
+     */
+    private void showBottomSheetDialog() {
+        if (behavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
+
+        //bottomSheetDialog의 layout설정
+        mBottomSheetDialog = new BottomSheetDialog(this);
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+
+        mBottomSheetDialog.setContentView(view);
+        mBottomSheetDialog.show();
+        mBottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                mBottomSheetDialog = null;
+            }
+        });
+    }
+}
